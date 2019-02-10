dc45e5f6bf2e67e56afcef0c4de21a4d98c674e0
Savannah  Greene <savannahgreene@Savannahs-MacBook-Pro-2.local>
2019-02-10 17:28:54 -0600

    Add hint messages into js file

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//1
+
 var boxes=document.querySelectorAll(".box")
 
 var highlights=["pink", "purple", "red"]
@@ -20,3 +22,36 @@ for (var i=0; i<boxes.length; i++) {
   boxes[i].addEventListener('mouseenter', toggleBackground)
   boxes[i].addEventListener('mouseleave', toggleBackground)
 }
+
+//2
+
+var yellowBox=document.querySelector(".yellow")
+
+function clickHint(e) {
+  var yellowHint =document.querySelector(".yellow p")
+  yellowHint.classList.toggle('hidden')
+}
+
+  yellowBox.addEventListener('click', clickHint);
+
+//3
+
+var blueBox=document.querySelector(".blue")
+
+function clickWinner(e) {
+  var blueWinner =document.querySelector(".blue p")
+  blueWinner.classList.toggle('hidden')
+}
+
+blueBox.addEventListener('click', clickWinner);
+
+//4
+
+var greenBox=document.querySelector(".green")
+
+function clickLoser(e) {
+  var greenLoser =document.querySelector(".green p")
+  greenLoser.classList.toggle('hidden')
+}
+
+greenBox.addEventListener('click', clickLoser);
